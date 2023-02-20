212777fe298f85170af48759b4c95a19d7fd7ad6
nica de leonÃ <vdeleon0000@gmail.com>
2023-02-20 17:47:10 -0300

    listoo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'game#run'
+  get "game", to: "game#run"
+  get "result", to: "game#score"
 end
