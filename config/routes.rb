4f956761ee6de8c89d8dcccfefb10c9d330ca703
nica de leonÃ <vdeleon0000@gmail.com>
2023-02-23 10:13:42 -0300

    terminado

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'game#run'
-  get "game", to: "game#run"
-  get "result", to: "game#score"
+  get "new", to: "games#new"
+  post "score", to: "games#score"
 end
