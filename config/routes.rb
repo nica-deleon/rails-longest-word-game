8bcaf2cbb427dd73ddbad59dd67e1434c1e99021
nica de leonÃ <vdeleon0000@gmail.com>
2023-02-23 10:16:42 -0300

    listo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "new", to: "games#new"
   post "score", to: "games#score"
 end
